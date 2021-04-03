e3dd85e9c2d90fb649453b36cd093cfcc080530d
Dmitry Pljonkin <dplenkin@lanit.ru>
2021-04-03 21:10:51 +0300

    Increased test coverage a bit

--- problems/palindromenumber/src/test/java/ru/sukebe/leetcode/problems/palindromenumber/SolutionTest.java
+++ problems/palindromenumber/src/test/java/ru/sukebe/leetcode/problems/palindromenumber/SolutionTest.java
@@ -8,16 +8,24 @@ class SolutionTest {
     @Test
     void testIsPalindrome() {
         Solution PalindromeNumberSolution = new Solution();
-        int x = 964737469;
-        boolean result = PalindromeNumberSolution.isPalindrome(x);
+        boolean result = PalindromeNumberSolution.isPalindrome(964737469);
+        assertEquals(true, result);
+
+        result = PalindromeNumberSolution.isPalindrome(1);
         assertEquals(true, result);
     }
 
     @Test
     void testIsNotPalindrome() {
         Solution PalindromeNumberSolution = new Solution();
-        int x = 964737468;
-        boolean result = PalindromeNumberSolution.isPalindrome(x);
+        boolean result = PalindromeNumberSolution.isPalindrome(964737468);
+        assertEquals(false, result);
+    }
+
+    @Test
+    void testNegativeIsNotPalindrome() {
+        Solution PalindromeNumberSolution = new Solution();
+        boolean result = PalindromeNumberSolution.isPalindrome(-1);
         assertEquals(false, result);
     }
 }
